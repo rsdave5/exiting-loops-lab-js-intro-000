dbf833e3c45b59abda2aaaedd857f4eac3dafdf0
Reetu Dave <rsdave5@gmail.com>
2019-11-10 00:37:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    array[i] = `changeValue`;
+    array[i] === `changeValue`;
     if (array[i] === 'stopValue') {
       break;
     }
